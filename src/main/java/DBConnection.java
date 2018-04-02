ad452973a56ac1e4e825122e5e6d309df6882e7e
Ulvbjern <dimich14@gmail.com>
2018-04-02 21:58:45 +0300

    Добавлено чтение более 1 файла и подключение вынесено в проперти

--- src/main/java/DBConnection.java
+++ src/main/java/DBConnection.java
@@ -1,19 +1,34 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 /**
  * Created by Dmitry on 23.02.2018.
  */
 public class DBConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/test";
-    private static final String PASS = "root";
-    private static final String LOGIN = "123qweasd";
+    //private static final String URL = "jdbc:mysql://localhost:3306/test";
+    //private static final String PASS = "root";
+    //private static final String LOGIN = "123qweasd";
+    Properties prop = new Properties();
+    FileInputStream fs;
     private Connection connection;
     public DBConnection(){
         try {
-            connection = DriverManager.getConnection(URL,PASS,LOGIN);
+            fs = new FileInputStream("application.properties");
+            prop.load(fs);
+            Class.forName(prop.getProperty("DB_DRIVER"));
+            connection = DriverManager.getConnection(prop.getProperty("URL"),prop.getProperty("LOGIN"),prop.getProperty("PASS"));
         }catch (SQLException e){
+            e.printStackTrace();
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+        }catch (IOException e){
+            e.printStackTrace();
+        }catch (ClassNotFoundException e){
             e.printStackTrace();
         }
     }
--- src/main/java/Insert2DB.java
+++ src/main/java/Insert2DB.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.PreparedStatement;
@@ -12,35 +13,46 @@ public class Insert2DB {
     public static void main(String[] args) {
         PreparedStatement pstm = null;
         DBConnection con;
-        try {
-            con = new DBConnection();
-            FileInputStream input = new FileInputStream("AKT OSAGO.xlsx");
-            XSSFWorkbook wb = new XSSFWorkbook(input);
-            XSSFSheet sheet = wb.getSheetAt(0);
-            Row row;
-            for (int i = 1; i <= sheet.getLastRowNum(); i++) {
-                row = sheet.getRow(i);
-                String REG_NUM = row.getCell(2).getStringCellValue();
-                String PSEVDONIM = row.getCell(3).getStringCellValue();
-                String FULL_NAME = row.getCell(4).getStringCellValue();
-                String STATUS = row.getCell(5).getStringCellValue();
-                String N_SVID_RSA = row.getCell(6).getStringCellValue();
-                String DT_SVID_RSA = row.getCell(7).getStringCellValue();
-                String ADRES = row.getCell(8).getStringCellValue();
-                String sql = "INSERT INTO test.acts_table VALUES('"+ null + "','" + null + "','" + REG_NUM + "','" + PSEVDONIM + "','" + FULL_NAME + "','" + STATUS + "','" + N_SVID_RSA + "','" + DT_SVID_RSA + "','" + ADRES + "','" + null +"','" + null +"','" + null +"','" + 0 +"','" + 0 +"','" + 0 +"')";
-                System.out.println(sql);
-                pstm = con.getConnection().prepareStatement(sql);
-                pstm.execute();
-                System.out.println("Import rows " + i);
+        File listFile = new File("F:\\ProjectJava\\MY tries\\DownloadExceltoDB\\loadfile");
+        File exportFiles[] = listFile.listFiles();
+        String[] names = new String[exportFiles.length];
+        for (int j = 0; j < names.length; j++) {
+            names[j] = exportFiles[j].getName();
+            System.out.println("Считываем файл" + ' ' + names[j]);
+
+
+            try {
+                con = new DBConnection();
+                FileInputStream input = new FileInputStream(listFile + "\\" + names[j]);
+                XSSFWorkbook wb = new XSSFWorkbook(input);
+                XSSFSheet sheet = wb.getSheetAt(0);
+                Row row;
+                for (int i = 1; i <= sheet.getLastRowNum(); i++) {
+                    row = sheet.getRow(i);
+                    System.out.println(row.getCell(2).getStringCellValue());
+                    String REG_NUM = row.getCell(2).getStringCellValue();
+                    String PSEVDONIM = row.getCell(3).getStringCellValue();
+                    String FULL_NAME = row.getCell(4).getStringCellValue();
+                    String STATUS = row.getCell(5).getStringCellValue();
+                    String N_SVID_RSA = row.getCell(6).getStringCellValue();
+                    String DT_SVID_RSA = row.getCell(7).getStringCellValue();
+                    String ADRES = row.getCell(8).getStringCellValue();
+                    String sql = "INSERT INTO test.acts_table VALUES('"+ null + "','" + null + "','" + REG_NUM + "','" + PSEVDONIM + "','" + FULL_NAME + "','" + STATUS + "','" + N_SVID_RSA + "','" + DT_SVID_RSA + "','" + ADRES + "','" + null +"','" + null +"','" + null +"','" + 0 +"','" + 0 +"','" + 0 +"')";
+                    System.out.println(sql);
+                    pstm = con.getConnection().prepareStatement(sql);
+                    pstm.execute();
+                    System.out.println("Import rows " + i);
+                }
+                pstm.close();
+                input.close();
+                System.out.println("Success import excel to mysql table");
+            } catch (SQLException ex) {
+                System.out.println(ex);
+            } catch (IOException ioe) {
+                System.out.println(ioe);
             }
-            pstm.close();
-            input.close();
-            System.out.println("Success import excel to mysql table");
-        } catch (SQLException ex) {
-            System.out.println(ex);
-        } catch (IOException ioe) {
-            System.out.println(ioe);
         }
+
 
     }
 }
